c10b9eb600f9ee08cb4d5360dbea9e9203dbdaac
hectormm1234 <hector.mell24@gmail.com>
2024-02-05 16:47:08 +0100

    Created an API with Flask

---
+++ requests_api.py
@@ -0,0 +1,14 @@
+import requests
+
+# Define the URL and query parameters
+url = 'http://127.0.0.1:5000/predict'
+params = {'sepal_length': 5.1,
+    'sepal_width': 3.5,
+    'petal_length': 1.4,
+    'petal_width': 0.2}
+
+# Make the GET request
+response = requests.get(url, params=params)
+
+# Print the response
+print(response.json())
