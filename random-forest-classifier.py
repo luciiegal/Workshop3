6a8123e26a5586a7c6c7e213029430a404b7e023
Louis Bessard <lbessard@Louiss-MacBook-Pro.local>
2024-02-12 16:40:06 +0100

    get has json answer 🙃

--- random-forest-classifier.py
+++ random-forest-classifier.py
@@ -1,26 +1,20 @@
-# Import necessary libraries
 from sklearn.datasets import load_iris
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import train_test_split
 import numpy as np
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, jsonify
 
-# Load the iris dataset
 iris = load_iris()
 X = iris.data
 y = iris.target
 labelsname = list(iris.target_names)
 
-# Split the dataset into training and testing sets
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.9, random_state=42)
 
-# Create a random forest classifier
 rf_classifier = RandomForestClassifier(n_estimators=100, random_state=42)
 
-# Train the classifier
 rf_classifier.fit(X_train, y_train)
 
-# Make predictions on the test set
 y_pred = rf_classifier.predict_proba(X_test)
 print("Predictions:", y_pred)
 
@@ -34,9 +28,6 @@ def read_root():
 
 @app.route("/predict/")
 def make_prediction():
-    """
-    Makes a prediction based on the input parameters and returns the result.
-    """
     try:
         L1 = float(request.args.get('L1'))
         W1 = float(request.args.get('W1'))
@@ -44,15 +35,19 @@ def make_prediction():
         W2 = float(request.args.get('W2'))
         
         testData = np.array([L1, W1, L2, W2]).reshape(-1, 4)
-        probalities = rf_classifier.predict_proba(testData)[0]
-        predicted = np.argmax(probalities)
-        probability = probalities[predicted]
+        probabilities = rf_classifier.predict_proba(testData)[0]
+        predicted = np.argmax(probabilities)
+        probability = probabilities[predicted]
         predicted = labelsname[predicted]
 
-        return render_template("prediction.html", probalities=probalities, predicted=predicted, probability=probability)
+        results = {label: prob for label, prob in zip(labelsname, probabilities)}
+        return jsonify(results)
+
+        #return render_template("prediction.html", probabilities=probabilities, predicted=predicted, probability=probability)
     
     except Exception as e:
-        return f"Error: {str(e)}"
+        return jsonify({"error": str(e)})
+        #return f"Error: {str(e)}"
 
 
 if __name__ == '__main__':
