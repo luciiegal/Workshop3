2fccac621d79d2ad027f1a7dd16de4bc7e0c7fb3
hectormm1234 <hector.mell24@gmail.com>
2024-02-19 22:16:12 +0100

    Updated API

--- requests_api.py
+++ requests_api.py
@@ -2,10 +2,10 @@ import requests
 
 # Define the URL and query parameters
 url = 'http://127.0.0.1:5000/predict'
-params = {'sepal_length': 5.1,
-    'sepal_width': 3.5,
-    'petal_length': 1.4,
-    'petal_width': 0.2}
+params = {'L1': 5.1,
+    'W1': 3.5,
+    'L2': 1.4,
+    'W2': 0.2}
 
 # Make the GET request
 response = requests.get(url, params=params)
